df4f37992ad4fc8bdde9fd5b6f3bc966deec4b51
Peter Kosyh <p.kosyh@gmail.com>
2014-03-04 15:24:27 +0000

    BBcode gish and spoiler

--- addons/plugins/BBCode/plugin.php
+++ addons/plugins/BBCode/plugin.php
@@ -45,6 +45,7 @@ public function handler_conversationController_renderBefore($sender)
  */
 public function handler_conversationController_getEditControls($sender, &$controls, $id)
 {
+	addToArrayString($controls, "spoiler", "<a href='javascript:BBCode.spoiler(\"$id\");void(0)' title='".T("Spoiler")."' class='bbcode-spoiler'><span>".T("Spoiler")."</span></a>", 0);
 	addToArrayString($controls, "fixed", "<a href='javascript:BBCode.fixed(\"$id\");void(0)' title='".T("Code")."' class='bbcode-fixed'><span>".T("Code")."</span></a>", 0);
 	addToArrayString($controls, "image", "<a href='javascript:BBCode.image(\"$id\");void(0)' title='".T("Image")."' class='bbcode-img'><span>".T("Image")."</span></a>", 0);
 	addToArrayString($controls, "link", "<a href='javascript:BBCode.link(\"$id\");void(0)' title='".T("Link")."' class='bbcode-link'><span>".T("Link")."</span></a>", 0);
@@ -63,10 +64,12 @@ public function handler_conversationController_getEditControls($sender, &$contro
 public function handler_format_beforeFormat($sender)
 {
 	$hideBlock = create_function('&$blockFixedContents, $contents', '
-		$blockFixedContents[] = $contents;
-		return "</p><pre></pre><p>";');
+		$geshi = new GeSHi(htmlspecialchars_decode($contents), "Lua", "/usr/share/php-geshi/geshi");
+		$blockFixedContents[] = $geshi->parse_code();
+		return "</p><pre><code></code></pre><p>";');
 	$hideInline = create_function('&$inlineFixedContents, $contents', '
-		$inlineFixedContents[] = $contents;
+		$geshi = new GeSHi(htmlspecialchars_decode($contents), "Lua", "/usr/share/php-geshi/geshi");
+		$inlineFixedContents[] = $geshi->parse_code();;
 		return "<code></code>";');
 
 	$this->blockFixedContents = array();
@@ -74,6 +77,7 @@ public function handler_format_beforeFormat($sender)
 
 	$regexp = "/(.*)^\s*\[code\]\n?(.*?)\n?\[\/code]$/imse";
 	while (preg_match($regexp, $sender->content)) {
+		include_once('/usr/share/php-geshi/geshi.php');
 		if ($sender->inline) $sender->content = preg_replace($regexp, "'$1' . \$hideInline(\$this->inlineFixedContents, '$2')", $sender->content);
 		else $sender->content = preg_replace($regexp, "'$1' . \$hideBlock(\$this->blockFixedContents, '$2')", $sender->content);
 	}
@@ -146,10 +150,10 @@ public function linksCallback($matches)
 public function handler_format_afterFormat($sender)
 {
 	// Retrieve the contents of the inline <code> tags from the array in which they are stored.
-	$sender->content = preg_replace("/<code><\/code>/ie", "'<code>' . array_shift(\$this->inlineFixedContents) . '</code>'", $sender->content);
-
+	if ($sender->inline) $sender->content = preg_replace("/<code><\/code>/ie", "'' . array_shift(\$this->inlineFixedContents) . ''", $sender->content);
 	// Retrieve the contents of the block <pre> tags from the array in which they are stored.
-	if (!$sender->inline) $sender->content = preg_replace("/<pre><\/pre>/ie", "'<pre>' . array_pop(\$this->blockFixedContents) . '</pre>'", $sender->content);
+	$sender->content = preg_replace("/<pre><code><\/code><\/pre>/ie", "'' . array_pop(\$this->blockFixedContents) . ''", $sender->content);
+
 }
 
 }
--- addons/plugins/BBCode/resources/bbcode.js
+++ addons/plugins/BBCode/resources/bbcode.js
@@ -1,5 +1,6 @@
 var BBCode = {
 
+spoiler: function(id) {ETConversation.wrapText($("#"+id+" textarea"), "[spoiler]", "[/spoiler]");},
 bold: function(id) {ETConversation.wrapText($("#"+id+" textarea"), "[b]", "[/b]");},
 italic: function(id) {ETConversation.wrapText($("#"+id+" textarea"), "[i]", "[/i]");},
 strikethrough: function(id) {ETConversation.wrapText($("#"+id+" textarea"), "[s]", "[/s]");},
