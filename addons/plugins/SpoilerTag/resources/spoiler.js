9e5b8f167c2e346d34722abad8472203f814b535
Peter Kosyh <p.kosyh@gmail.com>
2014-11-22 13:58:18 +0000

    SpoilerTag removed

--- addons/plugins/SpoilerTag/resources/spoiler.js
+++
@@ -1,9 +0,0 @@
-//$(document).ready(function() { 
-$(function() {
-	$(document).on("click",".spoiler span", function(){
-		$(this).siblings(".content").fadeToggle(600);
-	});
-	$(document).on("click",".nsfw span", function(){
-		$(this).siblings(".content").fadeToggle(600);
-	});
-});
